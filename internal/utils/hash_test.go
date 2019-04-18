1e6af1c345d41156d3ef9ccd5bec153abbef5fb1
Leonardo Grasso <me@leonardograsso.com>
2019-04-18 18:04:01 +0200

    chore: move file hashing func to internal

---
+++ internal/utils/hash_test.go
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2018-2019 vChain, Inc. All Rights Reserved.
+ * This software is released under GPL3.
+ * The full license information can be found under:
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ */
+
+package utils
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestHashFile(t *testing.T) {
+	file, err := ioutil.TempFile("", "example")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+	err = ioutil.WriteFile(file.Name(), []byte("123\n"), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	hash, retErr := HashFile(file.Name())
+	if hash != "181210f8f9c779c26da1d9b2075bde0127302ee0e3fca38c9a83f5b1dd8e5d3b" {
+		t.Error("hash does not match:", hash)
+	}
+
+	if retErr != nil {
+		t.Error("unexpected error returned:", err)
+	}
+}
