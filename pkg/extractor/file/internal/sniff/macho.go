6334f216ec2da46804ade9c3be25775efdaaa15f
Leonardo Grasso <me@leonardograsso.com>
2019-05-02 15:32:06 +0200

    chore: executable file sniffer

---
+++ pkg/extractor/file/internal/sniff/macho.go
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2018-2019 vChain, Inc. All Rights Reserved.
+ * This software is released under GPL3.
+ * The full license information can be found under:
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ */
+
+package sniff
+
+import (
+	"debug/macho"
+	"os"
+	"strings"
+)
+
+const Platform_MachO = "Mach"
+
+func MachO(file *os.File) (*Data, error) {
+	f, err := macho.NewFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	cpu := strings.TrimPrefix(f.Cpu.String(), "Cpu")
+
+	d := &Data{
+		Type:     f.Type.String(),
+		Platform: Platform_MachO,
+		Arch:     cpu,
+		X64:      strings.HasSuffix(cpu, "64"),
+	}
+	return d, nil
+}
---
+++ pkg/extractor/file/internal/sniff/pe.go
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2018-2019 vChain, Inc. All Rights Reserved.
+ * This software is released under GPL3.
+ * The full license information can be found under:
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ */
+
+package sniff
+
+import (
+	"debug/pe"
+	"os"
+)
+
+const Platform_PE = "Windows"
+
+var machineTypes = map[uint16]string{
+	pe.IMAGE_FILE_MACHINE_UNKNOWN:   "UNKNOWN",
+	pe.IMAGE_FILE_MACHINE_AM33:      "AM33",
+	pe.IMAGE_FILE_MACHINE_AMD64:     "AMD64",
+	pe.IMAGE_FILE_MACHINE_ARM:       "ARM",
+	pe.IMAGE_FILE_MACHINE_ARMNT:     "ARMNT",
+	pe.IMAGE_FILE_MACHINE_ARM64:     "ARM64",
+	pe.IMAGE_FILE_MACHINE_EBC:       "EBC",
+	pe.IMAGE_FILE_MACHINE_I386:      "I386",
+	pe.IMAGE_FILE_MACHINE_IA64:      "IA64",
+	pe.IMAGE_FILE_MACHINE_M32R:      "M32R",
+	pe.IMAGE_FILE_MACHINE_MIPS16:    "MIPS16",
+	pe.IMAGE_FILE_MACHINE_MIPSFPU:   "MIPSFPU",
+	pe.IMAGE_FILE_MACHINE_MIPSFPU16: "MIPSFPU16",
+	pe.IMAGE_FILE_MACHINE_POWERPC:   "POWERPC",
+	pe.IMAGE_FILE_MACHINE_POWERPCFP: "POWERPCFP",
+	pe.IMAGE_FILE_MACHINE_R4000:     "R4000",
+	pe.IMAGE_FILE_MACHINE_SH3:       "SH3",
+	pe.IMAGE_FILE_MACHINE_SH3DSP:    "SH3DSP",
+	pe.IMAGE_FILE_MACHINE_SH4:       "SH4",
+	pe.IMAGE_FILE_MACHINE_SH5:       "SH5",
+	pe.IMAGE_FILE_MACHINE_THUMB:     "THUMB",
+	pe.IMAGE_FILE_MACHINE_WCEMIPSV2: "WCEMIPSV2",
+}
+
+func PE(file *os.File) (*Data, error) {
+	f, err := pe.NewFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	arch := machineTypes[f.FileHeader.Machine]
+
+	x64 := false
+	switch f.OptionalHeader.(type) {
+	case *pe.OptionalHeader64:
+		x64 = true
+	}
+
+	format := "PE32"
+	if x64 {
+		format += "+"
+	}
+
+	d := &Data{
+		Format:   format,
+		Platform: Platform_PE,
+		Arch:     arch,
+		X64:      x64,
+		// Timestamp: f.TimeDateStamp,
+	}
+	return d, nil
+}
