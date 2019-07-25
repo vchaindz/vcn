2bfaeba19d6a8b0acacdb82193d4ed59244a2203
Leonardo Grasso <me@leonardograsso.com>
2019-07-25 18:44:08 +0200

    WIP allow to recover the secret even if present already

--- pkg/store/secret.go
+++ pkg/store/secret.go
@@ -16,7 +16,9 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
 func (u *User) keystore() (ks *keystore.KeyStore, err error) {
@@ -41,29 +43,38 @@ func (u *User) keystore() (ks *keystore.KeyStore, err error) {
 	return keystore.NewKeyStore(path, keystore.StandardScryptN, keystore.StandardScryptP), nil
 }
 
-func (u *User) PublicAddress() string {
+func (u *User) defaultAccount() (acc *accounts.Account, err error) {
 	ks, err := u.keystore()
 	if err != nil {
-		return ""
+		return
 	}
+
 	accs := ks.Accounts()
 	if len(accs) == 0 {
-		return ""
+		return
 	}
-	return strings.ToLower(accs[0].Address.Hex())
+
+	return &accs[0], nil
+}
+
+// PublicAddress returns the public key address for the User's secret, if any, otherwise an empty string.
+func (u *User) PublicAddress() string {
+	if acc, _ := u.defaultAccount(); acc != nil {
+		return strings.ToLower(acc.Address.Hex())
+	}
+	return ""
 }
 
 // OpenSecret opens the user's Web3 Secret Storage JSON file for reading.
 func (u *User) OpenSecret() (io.Reader, error) {
-	ks, err := u.keystore()
+	acc, err := u.defaultAccount()
 	if err != nil {
 		return nil, fmt.Errorf("cannot open the keystore: %s", err)
 	}
-	accs := ks.Accounts()
-	if len(accs) == 0 {
+	if acc == nil {
 		return nil, fmt.Errorf("no secret found in the keystore")
 	}
-	return os.Open(accs[0].URL.Path)
+	return os.Open(acc.URL.Path)
 }
 
 // ImportSecret imports the user's secret from a ECDSA private key and,
@@ -75,11 +86,26 @@ func (u *User) ImportSecret(privateKey ecdsa.PrivateKey, passphrase string) erro
 		return fmt.Errorf("cannot open the keystore: %s", err)
 	}
 
+	// todo(leogr): go-ethereum does not allow to overwrite an existing account,
+	// so we need to remove the stored secret if matches.
+	// But the previous stored account could be lost, although it can be still recovered
+	// by using the provided privateKey.
+	acc, err := ks.Find(accounts.Account{
+		Address: crypto.PubkeyToAddress(privateKey.PublicKey),
+	})
+	if err == nil {
+		if err := os.Remove(acc.URL.Path); err != nil {
+			return err
+		}
+		ks, _ = u.keystore() // reloads the keystore from disk
+	}
+
 	account, err := ks.ImportECDSA(&privateKey, passphrase)
 	if err != nil {
 		return err
 	}
 
+	// Cleanup other secrets, if any
 	for _, a := range ks.Accounts() {
 		if a.URL.Path == account.URL.Path {
 			continue
