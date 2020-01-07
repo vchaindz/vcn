0062426b8d84da996b0d1cf6005c2b69b7e6fd85
Leonardo Grasso <me@leonardograsso.com>
2020-01-07 15:35:55 +0100

    chore(cmd/internal/cli): reader and writer for YAML files

---
+++ pkg/cmd/internal/cli/yaml.go
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2018-2020 vChain, Inc. All Rights Reserved.
+ * This software is released under GPL3.
+ * The full license information can be found under:
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ */
+
+package cli
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+// WriteYAML writes _in_ data to a YAML file named by _filename_.
+func WriteYAML(in interface{}, filename string) error {
+	out, err := yaml.Marshal(in)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filename, out, 0644)
+}
+
+// ReadYAML reads the file named by _filename and assigns the decoded YAML content into the _out_ value.
+func ReadYAML(out interface{}, filename string) error {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	return yaml.Unmarshal(b, out)
+}
